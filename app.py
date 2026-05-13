a1bcbe60fcd20c908cefa2eac688cc0120f45b80
Václav Trávníček <Travnicek.Vaclav@student.gymzr.cz>
2026-05-13 12:18:58 +0200

    Templates

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
+
+
 @app.route("/", methods=["GET"])
 def page():
-	
-
-    
-    
     return render_template("page.html")
 
 
-if __name__=="__main__":
-	app.run(debug=True)
+@app.route("/vyhodnoceni", methods=["POST"])
+def vyhodnoceni():
+    jidlo = request.form.get("jidlo")
+    return render_template("page.html", jidlo=jidlo)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
